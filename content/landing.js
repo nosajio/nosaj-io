51c0dcdc66028395d2e626ea3ca079cf078964cc
Jason Howmans <jason@nosaj.io>
2017-05-31 01:52:26 +0100

    Further work to the landing view
    - Add dev script to package.
    - Add the logo.
    - Add arrow & plus .svg's.
    - More styling to landing top.
    - Split logo into its own include.

--- content/landing.js
+++ content/landing.js
@@ -1,4 +1,4 @@
 module.exports = () => ({
   title: 'Jason makes the internet',
-  introText: 'Hi, I\'m Jason. Nice to meet you! I specialise in helping people and companies build ambitious websites & apps quickly. My background is of both design and software engineering, so I am able to take ideas and turn them into products.'
+  introText: 'Hi there, I\'m Jason, nice to meet you. I am currently working to help people build ambitious websites & apps quickly. I specialise in taking ideas and rapidly turning them into products that can be measured and iterated on.'
 });
