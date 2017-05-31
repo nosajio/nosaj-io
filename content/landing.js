3576355c6fcd3d97e7ba322381f497be0dc21b9b
Jason Howmans <jason@nosaj.io>
2017-05-31 18:28:37 +0100

    Landing page colours

--- content/landing.js
+++ content/landing.js
@@ -1,6 +1,9 @@
 module.exports = () => ({
   title: 'Jason makes the internet',
-  introText: 'Hi there, I\'m Jason, nice to meet you. I am currently working to help people build ambitious websites & apps. I specialise in taking ideas and rapidly turning them into measureable products.',
+  introText: [
+    'I\'m Jason, nice to meet you. I design & make measurable products using modern web technologies.',
+    'I\'m currently working to help people build ambitious websites & apps to tight deadlines.'
+  ],
   available: true,
   quotes: [],
 });
