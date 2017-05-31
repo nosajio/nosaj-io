34cc1d218bf337d4a61ead8c07fb24989350bd1a
Jason Howmans <jason@nosaj.io>
2017-05-31 21:10:05 +0100

    Add content for Work page

--- content/work.js
+++ content/work.js
@@ -1,3 +1,22 @@
 module.exports = () => ({
-  
+  projects: [
+    {
+      product: 'Just A-level',
+      webURL: 'http://justalevel.com',
+      imageURL: '',
+      description: 'Just A-level is an education platform funded by the UK government as an initiative to teach computer science to more students. I was involved with the project from a very early stage, and was involved in the full process from planning and design to development. We were able to design and build the first version of the product in under two months, and successfully launched the site on time.'
+    },
+    {
+      product: 'BeachFix',
+      webURL: 'http://beachfix.co',
+      imageURL: '',
+      description: 'BeachFix is a beach holiday website that came about after the CEO spent a year travelling the world\'s beaches collecting data. I was hired to help the team design and build the first non-beta version of the app, condensing all of the lessons they had learned into a new version. We spent around a month on the design which was tested against user feedback, and another month on development.'
+    },
+    {
+      product: 'TopHat Films',
+      webURL: 'http://thetophatfilmswebsite.com',
+      imageURL: '',
+      description: 'The TopHat Films website is a fun site for filmmaker Rob Trott. I worked with him to embody the not-taking-ourselves-too-seriously image that TopHat are known for, while still maintaining usability and quality. The site is mostly vanilla HTML, CSS & JS, with some eastereggs thrown in for good measure.',
+    }
+  ]
 });
