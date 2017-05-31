2f109661633d9333678c5b59e89a482c128a4519
Jason Howmans <jason@nosaj.io>
2017-05-31 17:21:48 +0100

    Update intro content

--- content/landing.js
+++ content/landing.js
@@ -1,4 +1,6 @@
 module.exports = () => ({
   title: 'Jason makes the internet',
-  introText: 'Hi there, I\'m Jason, nice to meet you. I am currently working to help people build ambitious websites & apps quickly. I specialise in taking ideas and rapidly turning them into products that can be measured and iterated on.'
+  introText: 'Hi there, I\'m Jason, nice to meet you. I am currently working to help people build ambitious websites & apps. I specialise in taking ideas and rapidly turning them into measureable products.',
+  available: true,
+  quotes: [],
 });
