fa98df3622dc9c8b04df7a8e736f8e19948f9283
Jason Howmans <jason@nosaj.io>
2017-06-05 00:01:12 +0100

    UI all over
    - Add Navigation include and move in the logo
    - Add Stack into work content
    - Tighten up work page more

--- content/work.js
+++ content/work.js
@@ -13,32 +13,45 @@ module.exports = () => ({
         },
         {
           type: 'mobile',
-          url: 'http://a.nosaj.io/work/justalevel-dashboard-mobile.jpg'
+          url: 'http://a.nosaj.io/work/justalevel-course-mobile.jpg'
         },
       ],
-      description: 'Just A-level is an education platform funded by the UK government as an initiative to teach computer science to more students. I was involved with the project from a very early stage, and was involved in the full process from planning and design to development. We were able to design and build the first version of the product in under two months, and successfully launched the site on time.'
+      description: 'Just A-level is an education platform funded by the UK government as an initiative to teach computer science to more students. I was involved with the project from a very early stage, and was involved in the full process from planning and design to development. We were able to design and build the first version of the product in under two months, and successfully launched the site on time.',
+      stack: [
+        'NodeJS',
+        'Postgres',
+        'React',
+        'GraphQL'
+      ]
     },
     {
       id: 'beachfix',
       product: 'BeachFix',
-      webURL: 'http://beachfix.co',
+      webURL: 'http://www.beachfix.co/beaches/28',
       images: [
         {
           type: 'desktop',
-          url: 'http://a.nosaj.io/work/beachfix-beaches-desktop.jpg'
+          url: 'http://a.nosaj.io/work/beachfix-beach-desktop.jpg'
         },
         {
           type: 'mobile',
           url: 'http://a.nosaj.io/work/beachfix-beaches-mobile.jpg'
         },
       ],
-      description: 'BeachFix is a beach holiday website that came about after the CEO spent a year travelling the world\'s beaches collecting data. I was hired to help the team design and build the first non-beta version of the app, condensing all of the lessons they had learned into a new version. We spent around a month on the design which was tested against user feedback, and another month on development.'
+      description: 'BeachFix is a beach holiday website that came about after the CEO spent a year travelling the world\'s beaches collecting data. I was hired to help the team design and build the first non-beta version of the app, condensing all of the lessons they had learned into a new version. We spent around a month on the design which was tested against user feedback, and another month on development.',
+      stack: [
+        'NodeJS',
+        'React'
+      ]
     },
     {
       product: 'TopHat Films',
       webURL: 'http://thetophatfilmswebsite.com',
       images: [],
       description: 'The TopHat Films website is a fun site for filmmaker Rob Trott. I worked with him to embody the not-taking-ourselves-too-seriously image that TopHat are known for, while still maintaining usability and quality. The site is mostly vanilla HTML, CSS & JS, with some eastereggs thrown in for good measure.',
+      stack: [
+        'Vanilla'
+      ]
     }
   ]
 });
