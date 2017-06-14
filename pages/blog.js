3f3c43ea2c998fc078db58caa5ecd467fd297176
Jason Howmans <jason@nosaj.io>
2017-06-14 17:01:08 +0100

    Get post data into the template

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,8 @@
 const debug = require('debug')('nosaj:content:blog');
+const fileopener = require('../server/blog/file-opener');
+const markdown = require('../server/blog/markdown-parser');
 
 module.exports = (args) => {
-  debug(args);
   const headerData = {
     view: 'blog',
     path: '/r/:slug',
@@ -10,9 +11,40 @@ module.exports = (args) => {
     title: '',
     subtitle: '',
   };
-  return Object.assign(
-    {}, 
-    { post: null }, 
-    headerData
-  );
+  
+  // Return a stripped out version of the template before args are available
+  if (! args) {
+    return headerData;
+  }
+  
+  // Async
+  return new Promise(resolve => {
+    getPost(args.slug)
+      .then(post => {
+        const updatedData = { 
+          post,
+          title: post.title,
+        };
+        const allContent = Object.assign({}, headerData, updatedData);
+        resolve(allContent);
+      });
+  });
+}
+
+function getPost(slug) {
+  return new Promise(resolve => {
+    fileopener
+      .openAll()
+      .then((files) => {
+        const file = files.filter(f => markdown.parseFilename(f.name).slug === slug)[0];
+        const post = Object.assign(
+          {}, 
+          markdown.parseFilename(file.name), 
+          markdown.parseFile(file.body)
+        );
+        resolve(post);
+      }).catch(err => {
+        throw new Error(err.message)
+      });
+  });
 }
