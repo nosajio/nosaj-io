64a3cddd625deb82de64946da6cf4182184525ce
Jason Howmans <jason@nosaj.io>
2018-01-30 15:59:04 +0000

    Change how books page works underneath

--- pages/books.js
+++ pages/books.js
@@ -1,54 +1,31 @@
 const debug = require('debug')('nosaj:pages:books');
 const injectScripts = require('../lib/injectScripts');
 const { monthName } = require('../lib/helpers/date');
+const allBooks = require('../data/books');
 
 module.exports = (args) => ({
   view: 'books',
   path: '/books',
   stylesheet: 'views/books/books.scss',
   scripts: injectScripts(['views/books/books.js']),
-  title: 'Recommended Books',
+  title: 'Books',
   
   subtitle: 'A hand picked selection of books that I recommend',
   
-  // Books data
+  // Books information
   coversUrl: 'http://a.nosaj.io/books',
-  books: ({ coversUrl }) => [
-    {
-      img: `${coversUrl}/8020.jpg`,
-      title: 'The 80/20 Principle',
-      author: 'Richard Koch',
-      url: 'https://www.amazon.co.uk/80-20-Principle-Achieving-ANNIVERSARY/dp/1857886844/ref=pd_lpo_sbs_14_img_0?_encoding=UTF8&psc=1&refRID=6YZYERSST9KPXWH871E5',
-      readDate: new Date('2017/8'),
-      tags: [''],
-      comments: '',
-    },
-    {
-      img: `${coversUrl}/extreme-ownership.jpg`,
-      title: 'Extreme Ownership',
-      author: 'Jocko Willink and Leif Babin',
-      url: 'https://www.amazon.co.uk/Extreme-Ownership-Jocko-Willink/dp/1250183863/ref=sr_1_1?ie=UTF8&qid=1516966465&sr=8-1&keywords=extreme+ownership',
-      readDate: new Date('2017/6'),
-      tags: [''],
-      comments: '',
-    },
-    {
-      img: `${coversUrl}/power-of-habit.jpg`,
-      title: 'Power of Habit',
-      author: 'Charles Duhigg',
-      url: 'https://www.amazon.co.uk/Power-Habit-Why-What-Change/dp/1847946240/ref=sr_1_1?s=books&ie=UTF8&qid=1516966763&sr=1-1&keywords=power+of+habit',
-      readDate: new Date('2016/12'),
-      tags: [''],
-      comments: '',
-    },
-    {
-      img: `${coversUrl}/mastering-bitcoin.jpg`,
-      title: 'Mastering Bitcoin',
-      author: 'Andreas Antonopoulos',
-      url: 'https://www.amazon.co.uk/Mastering-Bitcoin-2e-Andreas-Antonopoulos/dp/1491954388/ref=sr_1_1?s=books&ie=UTF8&qid=1516967599&sr=1-1&keywords=mastering+bitcoin',
-      readDate: new Date('2017/10'),
-      tags: [''],
-      comments: '',
-    },
-  ]
+  bookCategories: {
+    startups: ['hackers-and-painters', '8020']
+  },
+
+  // Use the categories list to construct the full books object. Its done this way so that books aren't tied in to
+  // categories, so they can be moved around and stuff.
+  books: ({ bookCategories, coversUrl }) => {
+    const list = allBooks(coversUrl);
+    return Object.keys(bookCategories).map(c => ({
+      title: c,
+      list: list.filter(book => bookCategories[c].includes(book.id)),
+    }));
+  },
+
 });
