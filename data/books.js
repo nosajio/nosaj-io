63f165aec01525098977f87856279c8bee5548b0
Jason Howmans <jason@nosaj.io>
2018-01-31 21:34:18 +0000

    Better direction

--- data/books.js
+++ data/books.js
@@ -1,108 +1,92 @@
 module.exports = () => [
   {
     id: '8020',
-    isbn: '1857886844',
-    url: 'https://www.amazon.co.uk/80-20-Principle-Achieving-ANNIVERSARY/dp/1857886844/ref=pd_lpo_sbs_14_img_0?_encoding=UTF8&psc=1&refRID=6YZYERSST9KPXWH871E5',
+    title: '80/20 Principle',
   },
   {
     id: 'power-of-habit',
-    isbn: '1847946240',
+    title: 'The Power of Habit',
     buyUrl: 'https://www.amazon.co.uk/Power-Habit-Why-What-Change/dp/1847946240/ref=sr_1_1?s=books&ie=UTF8&qid=1516966763&sr=1-1&keywords=power+of+habit',
   },
   {
     id: 'lean-startup',
-    isbn: '0670921602',
+    title: 'The Lean Startup',
     buyUrl: 'https://www.amazon.co.uk/Lean-Startup-Innovation-Successful-Businesses/dp/0670921602/ref=sr_1_1?s=books&ie=UTF8&qid=1517405961&sr=1-1&keywords=lean+start',
   },
   {
     id: 'mastering-bitcoin',
-    isbn: '1491954388',
+    title: 'Mastering Bitcoin',
     buyUrl: 'https://www.amazon.co.uk/Mastering-Bitcoin-2e-Andreas-Antonopoulos/dp/1491954388/ref=sr_1_1?s=books&ie=UTF8&qid=1516967599&sr=1-1&keywords=mastering+bitcoin',
   },
   {
-    id: 'road-to-wigan-pier',
-    isbn: '0141185295',
-    buyUrl: 'https://www.amazon.co.uk/Road-Wigan-Penguin-Modern-Classics/dp/0141185295/ref=sr_1_1?s=books&ie=UTF8&qid=1517406127&sr=1-1&keywords=road+to+wigan+pier+george+orwell',
-  },
-  {
     id: 'tribes',
-    isbn: '0749939753',
+    title: 'Tribes',
     buyUrl: 'https://www.amazon.co.uk/Tribes-We-need-you-lead/dp/0749939753/ref=sr_1_1?s=books&ie=UTF8&qid=1517406450&sr=1-1&keywords=tribes',
   },
   {
-    id: 'letters-from-a-stoic',
-    isbn: '0140442103',
-    buyUrl: 'https://www.amazon.co.uk/Letters-Stoic-Epistulae-Lucilium-Classics/dp/0140442103/ref=sr_1_4?s=books&ie=UTF8&qid=1517407100&sr=1-4&keywords=letters+from+a+stoic'
-  },
-  {
     id: 'king-on-writing',
-    isbn: '1444723251',
+    title: 'Steven King on Writing',
     buyUrl: 'https://www.amazon.co.uk/Writing-Memoir-Craft-Stephen-King/dp/1444723251/ref=sr_1_2?s=books&ie=UTF8&qid=1517407210&sr=1-2&keywords=king+on+writing',
   },
   {
     id: 'elements-of-style',
-    isbn: '9780205309023',
+    title: 'Elements of Style',
     url: 'http://amzn.eu/eOttloX',
   },
   {
     id: 'politics-and-the-english-language',
-    isbn: '',
+    title: 'Politics and the English Language',
     url: '',
   },
   {
-    id: 'extreme-ownership',
-    isbn: '1250183863',
-    buyUrl: 'https://www.amazon.co.uk/Extreme-Ownership-Jocko-Willink/dp/1250183863/ref=sr_1_1?ie=UTF8&qid=1516966465&sr=8-1&keywords=extreme+ownership',
-  },
-  {
     id: 'hackers-and-painters',
-    isbn: '1449389554',
+    title: 'Hackers and Painters',
     buyUrl: 'https://www.amazon.co.uk/Hackers-Painters-Big-Ideas-Computer/dp/1449389554/ref=sr_1_1?s=books&ie=UTF8&qid=1517403042&sr=1-1&keywords=hackers+and+painters',
   },
   
   // {
   //   id: '',
-  //   isbn: '',
+  //   title: '',
   //   buyUrl: '',
   // },
   // {
   //   id: '',
-  //   isbn: '',
+  //   title: '',
   //   buyUrl: '',
   // },
   // {
   //   id: '',
-  //   isbn: '',
+  //   title: '',
   //   buyUrl: '',
   // },
   // {
   //   id: '',
-  //   isbn: '',
+  //   title: '',
   //   buyUrl: '',
   // },
   // {
   //   id: '',
-  //   isbn: '',
+  //   title: '',
   //   buyUrl: '',
   // },
   // {
   //   id: '',
-  //   isbn: '',
+  //   title: '',
   //   buyUrl: '',
   // },
   // {
   //   id: '',
-  //   isbn: '',
+  //   title: '',
   //   buyUrl: '',
   // },
   // {
   //   id: '',
-  //   isbn: '',
+  //   title: '',
   //   buyUrl: '',
   // },
   // {
   //   id: '',
-  //   isbn: '',
+  //   title: '',
   //   buyUrl: '',
   // },
 ];
