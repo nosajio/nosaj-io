8f4fb5acc31cff54a7bcb8f10dcbc4aafac0d30c
Jason Howmans <jason@nosaj.io>
2018-01-31 20:59:18 +0000

    Make the books page resolve book metadata
    Get the books data from google books api on each page load. This might
    be a bad way to do it, but it will be buffered by the nginx cache that
    runs on production.

--- data/books.js
+++ data/books.js
@@ -1,42 +1,108 @@
-module.exports = coversUrl => [
+module.exports = () => [
   {
     id: '8020',
-    img: `${coversUrl}/8020.jpg`,
-    title: 'The 80/20 Principle',
-    author: 'Richard Koch',
+    isbn: '1857886844',
     url: 'https://www.amazon.co.uk/80-20-Principle-Achieving-ANNIVERSARY/dp/1857886844/ref=pd_lpo_sbs_14_img_0?_encoding=UTF8&psc=1&refRID=6YZYERSST9KPXWH871E5',
-    readDate: new Date('2017/8'),
-    tags: [''],
-    comments: '',
-  },
-  {
-    id: 'extreme-ownership',
-    img: `${coversUrl}/extreme-ownership.jpg`,
-    title: 'Extreme Ownership',
-    author: 'Jocko Willink and Leif Babin',
-    url: 'https://www.amazon.co.uk/Extreme-Ownership-Jocko-Willink/dp/1250183863/ref=sr_1_1?ie=UTF8&qid=1516966465&sr=8-1&keywords=extreme+ownership',
-    readDate: new Date('2017/6'),
-    tags: [''],
-    comments: '',
   },
   {
     id: 'power-of-habit',
-    img: `${coversUrl}/power-of-habit.jpg`,
-    title: 'Power of Habit',
-    author: 'Charles Duhigg',
-    url: 'https://www.amazon.co.uk/Power-Habit-Why-What-Change/dp/1847946240/ref=sr_1_1?s=books&ie=UTF8&qid=1516966763&sr=1-1&keywords=power+of+habit',
-    readDate: new Date('2016/12'),
-    tags: [''],
-    comments: '',
+    isbn: '1847946240',
+    buyUrl: 'https://www.amazon.co.uk/Power-Habit-Why-What-Change/dp/1847946240/ref=sr_1_1?s=books&ie=UTF8&qid=1516966763&sr=1-1&keywords=power+of+habit',
+  },
+  {
+    id: 'lean-startup',
+    isbn: '0670921602',
+    buyUrl: 'https://www.amazon.co.uk/Lean-Startup-Innovation-Successful-Businesses/dp/0670921602/ref=sr_1_1?s=books&ie=UTF8&qid=1517405961&sr=1-1&keywords=lean+start',
   },
   {
     id: 'mastering-bitcoin',
-    img: `${coversUrl}/mastering-bitcoin.jpg`,
-    title: 'Mastering Bitcoin',
-    author: 'Andreas Antonopoulos',
-    url: 'https://www.amazon.co.uk/Mastering-Bitcoin-2e-Andreas-Antonopoulos/dp/1491954388/ref=sr_1_1?s=books&ie=UTF8&qid=1516967599&sr=1-1&keywords=mastering+bitcoin',
-    readDate: new Date('2017/10'),
-    tags: [''],
-    comments: '',
+    isbn: '1491954388',
+    buyUrl: 'https://www.amazon.co.uk/Mastering-Bitcoin-2e-Andreas-Antonopoulos/dp/1491954388/ref=sr_1_1?s=books&ie=UTF8&qid=1516967599&sr=1-1&keywords=mastering+bitcoin',
   },
+  {
+    id: 'road-to-wigan-pier',
+    isbn: '0141185295',
+    buyUrl: 'https://www.amazon.co.uk/Road-Wigan-Penguin-Modern-Classics/dp/0141185295/ref=sr_1_1?s=books&ie=UTF8&qid=1517406127&sr=1-1&keywords=road+to+wigan+pier+george+orwell',
+  },
+  {
+    id: 'tribes',
+    isbn: '0749939753',
+    buyUrl: 'https://www.amazon.co.uk/Tribes-We-need-you-lead/dp/0749939753/ref=sr_1_1?s=books&ie=UTF8&qid=1517406450&sr=1-1&keywords=tribes',
+  },
+  {
+    id: 'letters-from-a-stoic',
+    isbn: '0140442103',
+    buyUrl: 'https://www.amazon.co.uk/Letters-Stoic-Epistulae-Lucilium-Classics/dp/0140442103/ref=sr_1_4?s=books&ie=UTF8&qid=1517407100&sr=1-4&keywords=letters+from+a+stoic'
+  },
+  {
+    id: 'king-on-writing',
+    isbn: '1444723251',
+    buyUrl: 'https://www.amazon.co.uk/Writing-Memoir-Craft-Stephen-King/dp/1444723251/ref=sr_1_2?s=books&ie=UTF8&qid=1517407210&sr=1-2&keywords=king+on+writing',
+  },
+  {
+    id: 'elements-of-style',
+    isbn: '9780205309023',
+    url: 'http://amzn.eu/eOttloX',
+  },
+  {
+    id: 'politics-and-the-english-language',
+    isbn: '',
+    url: '',
+  },
+  {
+    id: 'extreme-ownership',
+    isbn: '1250183863',
+    buyUrl: 'https://www.amazon.co.uk/Extreme-Ownership-Jocko-Willink/dp/1250183863/ref=sr_1_1?ie=UTF8&qid=1516966465&sr=8-1&keywords=extreme+ownership',
+  },
+  {
+    id: 'hackers-and-painters',
+    isbn: '1449389554',
+    buyUrl: 'https://www.amazon.co.uk/Hackers-Painters-Big-Ideas-Computer/dp/1449389554/ref=sr_1_1?s=books&ie=UTF8&qid=1517403042&sr=1-1&keywords=hackers+and+painters',
+  },
+  
+  // {
+  //   id: '',
+  //   isbn: '',
+  //   buyUrl: '',
+  // },
+  // {
+  //   id: '',
+  //   isbn: '',
+  //   buyUrl: '',
+  // },
+  // {
+  //   id: '',
+  //   isbn: '',
+  //   buyUrl: '',
+  // },
+  // {
+  //   id: '',
+  //   isbn: '',
+  //   buyUrl: '',
+  // },
+  // {
+  //   id: '',
+  //   isbn: '',
+  //   buyUrl: '',
+  // },
+  // {
+  //   id: '',
+  //   isbn: '',
+  //   buyUrl: '',
+  // },
+  // {
+  //   id: '',
+  //   isbn: '',
+  //   buyUrl: '',
+  // },
+  // {
+  //   id: '',
+  //   isbn: '',
+  //   buyUrl: '',
+  // },
+  // {
+  //   id: '',
+  //   isbn: '',
+  //   buyUrl: '',
+  // },
 ];
--- pages/books.js
+++ pages/books.js
@@ -1,7 +1,8 @@
 const debug = require('debug')('nosaj:pages:books');
+const error = require('debug')('nosaj:error:pages:books');
 const injectScripts = require('../lib/injectScripts');
-const { monthName } = require('../lib/helpers/date');
 const allBooks = require('../data/books');
+const resolveBooks = require('../lib/helpers/books');
 
 module.exports = (args) => ({
   view: 'books',
@@ -12,8 +13,7 @@ module.exports = (args) => ({
   
   subtitle: 'We should all do more reading, but it can be hard to find what to read next. Here’s a selection of books that I recommend to people all the time.',
   
-  // Books information
-  coversUrl: 'http://a.nosaj.io/books',
+  // Books data
   bookCategories: {
     "Making": ['hackers-and-painters'],
     "Design": [],
@@ -22,12 +22,17 @@ module.exports = (args) => ({
 
   // Use the categories list to construct the full books object. Its done this way so that books aren't tied in to
   // categories, so they can be moved around and stuff.
-  books: ({ bookCategories, coversUrl }) => {
-    const list = allBooks(coversUrl);
-    return Object.keys(bookCategories).map(c => ({
-      title: c,
-      list: list.filter(book => bookCategories[c].includes(book.id)),
-    }));
-  },
+  books: ({ bookCategories }) => new Promise(resolve => {
+    const list = allBooks();
+    return resolveBooks(list)
+      .then(books => {
+        const categoriesWithBooks = Object.keys(bookCategories).map(c => ({
+          category: c,
+          titles: books.filter(book => bookCategories[c].includes(book.local.id)),
+        }));
+        return resolve(categoriesWithBooks);
+      })
+      .catch(err => error(err));
+  }),
 
 });
