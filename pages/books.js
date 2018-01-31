dd4e5b8ce844395695e25c91a8607ff3f0df0024
Jason Howmans <jason@nosaj.io>
2018-01-31 21:34:18 +0000

    Better direction

--- pages/books.js
+++ pages/books.js
@@ -2,7 +2,6 @@ const debug = require('debug')('nosaj:pages:books');
 const error = require('debug')('nosaj:error:pages:books');
 const injectScripts = require('../lib/injectScripts');
 const allBooks = require('../data/books');
-const resolveBooks = require('../lib/helpers/books');
 
 module.exports = (args) => ({
   view: 'books',
@@ -14,6 +13,7 @@ module.exports = (args) => ({
   subtitle: 'We should all do more reading, but it can be hard to find what to read next. Here’s a selection of books that I recommend to people all the time.',
   
   // Books data
+  coversUrl: 'http://a.nosaj.io/books',
   bookCategories: {
     "Making": ['hackers-and-painters'],
     "Design": [],
@@ -22,17 +22,15 @@ module.exports = (args) => ({
 
   // Use the categories list to construct the full books object. Its done this way so that books aren't tied in to
   // categories, so they can be moved around and stuff.
-  books: ({ bookCategories }) => new Promise(resolve => {
-    const list = allBooks();
-    return resolveBooks(list)
-      .then(books => {
-        const categoriesWithBooks = Object.keys(bookCategories).map(c => ({
-          category: c,
-          titles: books.filter(book => bookCategories[c].includes(book.local.id)),
-        }));
-        return resolve(categoriesWithBooks);
-      })
-      .catch(err => error(err));
-  }),
+  books: ({ bookCategories, coversUrl }) => {
+    const books = allBooks();
+    const booksWithCovers = books.map(b => Object.assign({}, b, { img: `${coversUrl}/${b.id}.jpg` }));
+    debug(booksWithCovers)
+    const categoriesWithBooks = Object.keys(bookCategories).map(c => ({
+      category: c,
+      titles: booksWithCovers.filter(book => bookCategories[c].includes(book.id)),
+    }));
+    return categoriesWithBooks;
+  },
 
 });
