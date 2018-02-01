37d3121bf62da8eb21b5f1fa22f9577010eb16c5
Jason Howmans <jason@nosaj.io>
2018-02-01 00:52:49 +0000

    Good time to stop for the night

--- pages/books.js
+++ pages/books.js
@@ -10,27 +10,17 @@ module.exports = (args) => ({
   scripts: injectScripts(['views/books/books.js']),
   title: 'Books',
   
-  subtitle: 'We should all do more reading, but it can be hard to find what to read next. Here’s a selection of books that I recommend to people all the time.',
+  subtitle: ['This page is part reading diary and part recommended reading list. I only add a book to this page if I\'d read it again.', 'Take a browse, you might find something you like :)'],
   
   // Books data
   coversUrl: 'http://a.nosaj.io/books',
-  bookCategories: {
-    "Making": ['hackers-and-painters'],
-    "Design": [],
-    "Startups": ['8020']
-  },
 
   // Use the categories list to construct the full books object. Its done this way so that books aren't tied in to
   // categories, so they can be moved around and stuff.
   books: ({ bookCategories, coversUrl }) => {
     const books = allBooks();
     const booksWithCovers = books.map(b => Object.assign({}, b, { img: `${coversUrl}/${b.id}.jpg` }));
-    debug(booksWithCovers)
-    const categoriesWithBooks = Object.keys(bookCategories).map(c => ({
-      category: c,
-      titles: booksWithCovers.filter(book => bookCategories[c].includes(book.id)),
-    }));
-    return categoriesWithBooks;
+    return booksWithCovers;
   },
 
 });
