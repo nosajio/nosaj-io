f2c01f703ed46d0b7a18feab81452f10dd8a89bc
Jason Howmans <jason@nosaj.io>
2018-02-01 00:52:49 +0000

    Good time to stop for the night

--- data/books.js
+++ data/books.js
@@ -1,5 +1,30 @@
 module.exports = () => [
   {
+    id: 'mastering-bitcoin',
+    title: 'Mastering Bitcoin',
+    buyUrl: 'https://www.amazon.co.uk/Mastering-Bitcoin-2e-Andreas-Antonopoulos/dp/1491954388/ref=sr_1_1?s=books&ie=UTF8&qid=1516967599&sr=1-1&keywords=mastering+bitcoin',
+  },
+  {
+    id: 'wisdom-of-crowds',
+    title: 'Wisdom of Crowds',
+    buyUrl: '',
+  },
+  {
+    id: 'synthesis-of-form',
+    title: 'Notes on the Synthesis of Form',
+    buyUrl: '',
+  },
+  {
+    id: 'information-is-beautiful',
+    title: 'Information is Beautiful',
+    buyUrl: '',
+  },
+  {
+    id: 'visual-display',
+    title: 'The Visual Display of Quantitative Information',
+    buyUrl: '',
+  },
+  {
     id: '8020',
     title: '80/20 Principle',
   },
@@ -14,9 +39,34 @@ module.exports = () => [
     buyUrl: 'https://www.amazon.co.uk/Lean-Startup-Innovation-Successful-Businesses/dp/0670921602/ref=sr_1_1?s=books&ie=UTF8&qid=1517405961&sr=1-1&keywords=lean+start',
   },
   {
-    id: 'mastering-bitcoin',
-    title: 'Mastering Bitcoin',
-    buyUrl: 'https://www.amazon.co.uk/Mastering-Bitcoin-2e-Andreas-Antonopoulos/dp/1491954388/ref=sr_1_1?s=books&ie=UTF8&qid=1516967599&sr=1-1&keywords=mastering+bitcoin',
+    id: 'hackers-and-painters',
+    title: 'Hackers and Painters',
+    buyUrl: 'https://www.amazon.co.uk/Hackers-Painters-Big-Ideas-Computer/dp/1449389554/ref=sr_1_1?s=books&ie=UTF8&qid=1517403042&sr=1-1&keywords=hackers+and+painters',
+  },
+  {
+    id: 'book-of-five-rings',
+    title: 'Book of Five Rings',
+    buyUrl: '',
+  },
+  {
+    id: 'structures',
+    title: 'Structures',
+    buyUrl: '',
+  },
+  {
+    id: 'essentialism',
+    title: 'Essentialism',
+    buyUrl: '',
+  },
+  {
+    id: 'hooked',
+    title: 'Hooked',
+    buyUrl: '',
+  },
+  {
+    id: 'antifragile',
+    title: 'Antifragile',
+    buyUrl: '',
   },
   {
     id: 'tribes',
@@ -39,51 +89,11 @@ module.exports = () => [
     url: '',
   },
   {
-    id: 'hackers-and-painters',
-    title: 'Hackers and Painters',
-    buyUrl: 'https://www.amazon.co.uk/Hackers-Painters-Big-Ideas-Computer/dp/1449389554/ref=sr_1_1?s=books&ie=UTF8&qid=1517403042&sr=1-1&keywords=hackers+and+painters',
+    id: 'lying',
+    title: 'Lying',
+    buyUrl: '',
   },
-  
-  // {
-  //   id: '',
-  //   title: '',
-  //   buyUrl: '',
-  // },
-  // {
-  //   id: '',
-  //   title: '',
-  //   buyUrl: '',
-  // },
-  // {
-  //   id: '',
-  //   title: '',
-  //   buyUrl: '',
-  // },
-  // {
-  //   id: '',
-  //   title: '',
-  //   buyUrl: '',
-  // },
-  // {
-  //   id: '',
-  //   title: '',
-  //   buyUrl: '',
-  // },
-  // {
-  //   id: '',
-  //   title: '',
-  //   buyUrl: '',
-  // },
-  // {
-  //   id: '',
-  //   title: '',
-  //   buyUrl: '',
-  // },
-  // {
-  //   id: '',
-  //   title: '',
-  //   buyUrl: '',
-  // },
+ 
   // {
   //   id: '',
   //   title: '',
